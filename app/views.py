dfe387acd6f5f5d64c73a4a038bf817e9a149449
NatashaSenah <tashsenah@gmail.com>
2018-08-31 16:35:33 +0300

    add the created news

--- app/views.py
+++ app/views.py
@@ -5,7 +5,8 @@ from app import app
 @app.route('/')
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('index.html')
+    message = 'NEWS HIGHLIGHT'
+    return render_template('index.html',message = message)
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    return render_template('news.html',id = news_id)
