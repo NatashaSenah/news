4c203781f819187eceb723690698621be864f8ce
NatashaSenah <tashsenah@gmail.com>
2018-09-03 20:15:36 +0300

    add the modified news

--- app/news_test.py
+++ app/news_test.py
@@ -7,7 +7,7 @@ class NewsTest(unittest.TestCase):
    
     def setUp(self):
         
-        self.new_news = News(1234,'ABCNews','A government officer being imprissoned','general')
+        self.new_news = News(12345,'ABCNews','A government officer being imprissoned','general','Kenya')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
