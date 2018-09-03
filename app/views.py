36a5a6072eca1aafb80c3a3ea1ea013c28d77761
NatashaSenah <tashsenah@gmail.com>
2018-09-03 19:28:37 +0300

    add the modified app/views.py

--- app/views.py
+++ app/views.py
@@ -15,12 +15,6 @@ def index():
     title = 'Home - Welcome to The best News Highlight Website Online'
     return render_template('index.html', title = title, general = general_news,business=business_news,technology=technology_news,sports=sports_news,entertainment=entertainment_news)
 
-
-# @app.route('/news/<int:news_id>')
-# def news(news_id):
-#     news=get_news(id)
-#     title  = f'You are viewing{news_id}'
-#     return render_template('news.html',id = news_id)
 @app.route('/news/<string:id>&<int:page_size>')
 def articles(id,page_size):
 
